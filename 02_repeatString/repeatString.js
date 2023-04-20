09d76606889f3c278356947158651ce8f10dde43
hlokman <abdallahlokmanhenni@gmail.com>
2023-04-20 16:45:01 +0200

    Solve the exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    let chosenWord = '';
+    
+    if (times < 0) {
+        return 'ERROR'
+    };
+
+    for (let i=1; i<=times; i++) {
+        chosenWord += word
+    }
+    return chosenWord;
+
 
 };
 
