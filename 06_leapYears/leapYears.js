a4abf070d4886c239330ebb2a11a0e92d9494557
hlokman <abdallahlokmanhenni@gmail.com>
2023-04-24 22:00:27 +0200

    Solve the exercise (using 2 methods)

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (Number.isInteger(year/4) && !Number.isInteger(year/100) || Number.isInteger(year/100) && Number.isInteger(year/400)) {
+        return true;
+    } else {
+        return false;
+    } 
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+/*   
+        if (year % 4 == 0 && year % 100 != 0 || year % 100 == 0 && year % 400 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+    */
