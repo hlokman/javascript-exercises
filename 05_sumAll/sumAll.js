513bfe6a532476daee40bce695f9a1b998a79d50
hlokman <abdallahlokmanhenni@gmail.com>
2023-04-24 20:43:35 +0200

    Solve the exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(numA, numB) {
+    let finalSum=0;
 
+    if (numA<=numB) {
+        for (let i=numA; i<=numB; i++ ) {
+            finalSum += i;
+        } 
+    } else {
+        for (let i=numB; i<=numA; i++) {
+            finalSum += i;
+        }
+    };
+
+
+    if (numA < 0 || numB < 0 || !Number.isInteger(numA) || !Number.isInteger(numB)) {
+        return 'ERROR';
+    };
+
+    return finalSum;
 };
 
 // Do not edit below this line
