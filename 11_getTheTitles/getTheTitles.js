bfd14b6b55c0392c736216d6ada2bbc36cfeadeb
hlokman <abdallahlokmanhenni@gmail.com>
2023-05-20 23:38:23 +0200

    Solve the exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    return array.map(item => 
+        item.title
+    )
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+/*
+let result = [];
+
+    array.filter((item) => {
+        if (item.title) {
+            result.push(item.title);
+        }
+    });
+    return result;*/
