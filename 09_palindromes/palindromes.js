a4ed815fdff130459931df171ea3fb390ce5965e
hlokman <abdallahlokmanhenni@gmail.com>
2023-05-20 22:19:19 +0200

    Solve the exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    if (string.split('').reverse().join('').replace(/[^a-zA-Z ]/g, "").replace(/ /g,'').toLowerCase() == string.replace(/[^a-zA-Z ]/g, "").replace(/ /g,'').toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
