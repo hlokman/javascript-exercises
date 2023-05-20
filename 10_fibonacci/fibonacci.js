ac6efcefa1fdcbd925fc340a34d0f1dcd65a3b41
hlokman <abdallahlokmanhenni@gmail.com>
2023-05-20 23:02:11 +0200

    Solve the exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fib = [0, 1];
 
+    if (Number(num) >= 0) {
+        for (let i = 2; i <= Number(num); i++) {
+            fib.splice(i, 0, fib[i - 1] + fib[i - 2]); 
+        };
+    } else {
+        return "OOPS"
+    };
+
+    return fib[Number(num)];
 };
 
 // Do not edit below this line
